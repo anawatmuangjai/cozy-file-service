ddd1e582b6e76ff8922f748a98b77ad7cb3a26b8
Anawat Muangjai <anawat.muangjai@gmail.com>
2024-03-22 12:33:35 +0700

    Add infrastructure project file

---
+++ src/CozyFileService.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,21 @@
+﻿using CozyFileService.Application.Contracts.Infrastructure;
+using CozyFileService.Application.Models.Mail;
+using CozyFileService.Infrastructure.Mail;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CozyFileService.Infrastructure
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            var emailSettings = configuration.GetSection("EmailSettings");
+            services.Configure<EmailSettings>(emailSettings);
+
+            services.AddTransient<IEmailService, EmailService>();
+
+            return services;
+        }
+    }
+}
---
+++ src/CozyFileService.Infrastructure/Mail/EmailService.cs
@@ -0,0 +1,44 @@
+﻿using CozyFileService.Application.Contracts.Infrastructure;
+using CozyFileService.Application.Models.Mail;
+using Microsoft.Extensions.Options;
+using SendGrid.Helpers.Mail;
+using SendGrid;
+
+namespace CozyFileService.Infrastructure.Mail
+{
+    public class EmailService : IEmailService
+    {
+        public EmailSettings _emailSettings { get; set; }
+
+        public EmailService(IOptions<EmailSettings> mailSettings)
+        {
+            _emailSettings = mailSettings.Value;
+        }
+
+        public async Task<bool> SendEmailAsync(Email email)
+        {
+            var client = new SendGridClient(_emailSettings.ApiKey);
+
+            var subject = email.Subject;
+            var to = new EmailAddress(email.To);
+            var emailBody = email.Body;
+
+            var from = new EmailAddress
+            {
+                Email = _emailSettings.FromAddress,
+                Name = _emailSettings.FromName,
+            };
+
+            var sendGridMessage = MailHelper.CreateSingleEmail(from, to, subject, emailBody, emailBody);
+
+            var response = await client.SendEmailAsync(sendGridMessage);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.Accepted || response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
